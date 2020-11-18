03d3b5cdcc334606db89c19c80b3ee8ca96be435
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-11-18 18:05:43 +0100

    Fetching the house listing data

--- redux/actions/houseAction.js
+++ redux/actions/houseAction.js
@@ -3,9 +3,13 @@ export const FETCH_HOUSES = 'FETCH_HOUSES';
 export const CREATE_HOUSES = 'CREATE_HOUSES';
 
 export const fetchHouses = () => {
-	// add logic to fetch houses from API
 	// dispatch function to fetchHouses function
 	return async (dispatch) => {
+		// add logic to fetch houses from API
+		const result = await fetch('http://192.168.1.63:3000/api/houses');
+		const resultData = await result.json();
+		// console.log(resultData);
+
 		// action needs to return an object with type and payload
 		dispatch({
 			type: FETCH_HOUSES,
--- screens/HomeListScreen.js
+++ screens/HomeListScreen.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
 // import the FloatingAction component from the package
 import { FloatingAction } from 'react-native-floating-action';
 
+// useDispatch to use the dispatch from action
+import { useDispatch } from 'react-redux';
+
 // import card component
 import Card from '../components/Card';
 
+// import all actions from houseAction
+import * as houseACtion from '../redux/actions/houseAction';
+
 const HomeListScreen = (props) => {
+	// init useDispatch
+	const dispatch = useDispatch();
+
+	// dispatch action when components renders so we need useEffect
+	useEffect(() => {
+		dispatch(houseACtion.fetchHouses());
+	}, [dispatch]);
+
 	return (
 		<View style={styles.container}>
 			<Card navigation={props.navigation} />
