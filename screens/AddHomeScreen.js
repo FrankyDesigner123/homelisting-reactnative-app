8e9127562cc25e7604867ef225ac7165ac2bf5a3
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-11-18 12:25:13 +0100

    Building out the Add Home Screen

--- screens/AddHomeScreen.js
+++ screens/AddHomeScreen.js
@@ -1,13 +1,78 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import {
+	StyleSheet,
+	View,
+	Text,
+	ScrollView,
+	TextInput,
+	Button,
+} from 'react-native';
 
 const AddHomeScreen = () => {
 	return (
-		<View>
-			<Text>Add Home Screen</Text>
-		</View>
+		<ScrollView>
+			<View style={styles.form}>
+				<View style={styles.formGroup}>
+					<Text style={styles.label}>Title:</Text>
+					<TextInput style={styles.input} />
+				</View>
+
+				<View style={styles.formGroup}>
+					<Text style={styles.label}>Image URL:</Text>
+					<TextInput style={styles.input} />
+				</View>
+
+				<View style={styles.formGroup}>
+					<Text style={styles.label}>HomeType:</Text>
+					<TextInput style={styles.input} />
+				</View>
+
+				<View style={styles.formGroup}>
+					<Text style={styles.label}>Price:</Text>
+					<TextInput style={styles.input} />
+				</View>
+				<View style={styles.formGroup}>
+					<Text style={styles.label}>Year Built:</Text>
+					<TextInput multiline style={styles.input} />
+				</View>
+				<View style={styles.formGroup}>
+					<Text style={styles.label}>Address:</Text>
+					<TextInput style={styles.input} />
+				</View>
+				<View style={styles.formGroup}>
+					<Text style={styles.label}>Description</Text>
+					<TextInput multiline style={styles.input} />
+				</View>
+
+				<View>
+					<Button title="Add Home" style={styles.buttonContainer} />
+				</View>
+			</View>
+		</ScrollView>
 	);
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	form: {
+		margin: 20,
+		backgroundColor: '#fff',
+		padding: 20,
+		borderRadius: 10,
+	},
+	formGroup: {
+		width: '100%',
+	},
+	label: {
+		marginVertical: 10,
+	},
+	input: {
+		paddingHorizontal: 2,
+		paddingVertical: 8,
+		borderBottomColor: '#ccc',
+		borderBottomWidth: 1,
+	},
+	buttonContainer: {
+		marginTop: 20,
+	},
+});
 
 export default AddHomeScreen;
