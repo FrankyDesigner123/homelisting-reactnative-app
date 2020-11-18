1771156b8094a3e369fb4de65b8e54dae40b24d5
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-11-18 10:32:53 +0100

    Setting up the react native app

---
+++ screens/HomeListScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const HomeListScreen = () => {
+	return (
+		<View>
+			<Text>About Screen</Text>
+		</View>
+	);
+};
+const styles = StyleSheet.create({});
+
+export default HomeListScreen;
